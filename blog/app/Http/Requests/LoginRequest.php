d9016a137ee5bb653439cc2d8391916682ca1cad
dmitrii-liashko <kreedo5.2002@gmail.com>
2026-09-18 17:33:58 +0300

    FormRequest-ы регистрации и авторизации

---
+++ blog/app/Http/Requests/LoginRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+         // Авторизация доступна для всех
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'email'],
+            'password' => ['required', 'string'],
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'email' => 'Email',
+            'password' => 'Пароль',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'Требуется указать email.',
+            'email.email' => 'Указан некорректный email.',
+            'password.required' => 'Требуется указать пароль.',
+        ];
+    }
+}
---
+++ blog/app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        // Регистрация доступна для всех
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:4'],
+        ];
+    }
+
+    public function attributes(): array
+    {
+        // Имена полей — для текста ошибок
+        return [
+            'name' => 'Имя',
+            'email' => 'Email',
+            'password' => 'Пароль',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Требуется указать имя.',
+            'name.string' => 'Имя должно быть строкой.',
+            'name.max' => 'Имя не должно превышать :max символов.',
+            'email.required' => 'Требуется указать email.',
+            'email.email' => 'Указан некорректный email.',
+            'email.max' => 'Email не должен превышать :max символов.',
+            'email.unique' => 'Пользователь с указанным email уже зарегистрирован.',
+            'password.required' => 'Требуется указать пароль.',
+            'password.string' => 'Пароль должен быть строкой.',
+            'password.min' => 'Пароль должен содержать не менее :min символов.',
+        ];
+    }
+}
