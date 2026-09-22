6df01eb104e200f19368add812f9fc8295e0b3c0
dmitrii-liashko <kreedo5.2002@gmail.com>
2026-09-22 09:57:16 +0300

    Добавил транзакцию при неудачной регистрации (на случай если токен не сгенерировался, а пользователь был создан в бд)

--- blog/app/Services/Auth/RegisterUserService.php
+++ blog/app/Services/Auth/RegisterUserService.php
@@ -5,17 +5,23 @@ namespace App\Services\Auth;
 use App\Exceptions\ApiException;
 use App\Models\User;
 use Illuminate\Database\UniqueConstraintViolationException;
+use Illuminate\Support\Facades\DB;
+use Throwable;
 
 class RegisterUserService
 {
     public function create(array $data): array
     {
         try {
-            $user = User::create($data);
+            return DB::transaction(function () use ($data): array {
+                $user = User::create($data);
+
+                return ['user' => $user, 'accessToken' => $user->generateToken()];
+            });
         } catch (UniqueConstraintViolationException) {
             throw new ApiException('Пользователь с таким email уже зарегистрирован.', 409);
+        } catch (Throwable) {
+            throw new ApiException('Не удалось зарегистрировать пользователя, попробуйте позже.', 500);
         }
-
-        return ['user' => $user, 'accessToken' => $user->generateToken()];
     }
 }
